edf621f8a92d9eab2e5aac09c612d28a4ae64168
rap2h <raphaelht@gmail.com>
2019-05-20 14:34:23 +0200

    active filters

--- src/ActiveFilters.js
+++ src/ActiveFilters.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { useSharedContext } from "./SharedContextProvider";
 
-export default function({ item }) {
+export default function({ items }) {
   const [{ widgets }, dispatch] = useSharedContext();
-  // Get all values from widgets.
-  const values = new Map([...widgets].filter(([, v]) => v.value).map(([k, v]) => [k, v.value]));
+  const activeFilters = [...widgets]
+    .filter(([, v]) => (Array.isArray(v.value) ? v.value.length : v.value))
+    .map(([k, v]) => ({
+      key: k,
+      value: Array.isArray(v.value) ? v.value.join(", ") : v.value
+    }));
 
   // On filter remove, update widget properties.
   function removeFilter(id) {
@@ -17,18 +21,22 @@ export default function({ item }) {
     });
   }
 
-  const items = [...values]
-    .filter(([, v]) => (Array.isArray(v) ? v.length : v))
-    .map(([k, v]) => {
-      return (
-        <li key={k}>
-          {item ? item(k, v) : <span>{`${k}: ${v}`}</span>}
-          <button onClick={() => removeFilter(k)}>
-            x
-          </button>
-        </li>
-      );
-    });
-
-  return <div className="react-es-active-filters">{items.length ? <ul>{items}</ul> : <div />}</div>;
+  return (
+    <div className="react-es-active-filters">
+      {items ? (
+        items(activeFilters, removeFilter)
+      ) : (
+        <ul>
+          {activeFilters.map(({ key, value }) => {
+            return (
+              <li key={key}>
+                <span>{`${key}: ${value}`}</span>
+                <button onClick={() => removeFilter(key)}>x</button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 }
