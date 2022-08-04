5d13655cbfdb724160545fd3e24505ed23f768b5
danjer <d.h.vandenakker@live.nl>
2022-08-04 10:23:19 +0200

    wip

--- src/GeoFacet.js
+++ src/GeoFacet.js
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { toTermQueries } from "./utils";
 import { useSharedContext } from "./SharedContextProvider";
+import { isArray } from "lodash";
+
+const constructGeoQuery = (field, shapeValue) => {
+    if (Array.isArray(shapeValue)) {
+        return { bool: { should: [] } }
+    } else {
+        return {
+            geo_shape: {
+                [field]: {
+                    shape: shapeValue,
+                    relation: "within"
+                },
+            }
+        }
+    }
+}
 
 export default function ({
-    fields,
     id,
     initialValue,
-    seeMore,
-    placeholder,
-    showFilter = true,
-    filterValueModifier,
-    items
+    field
 }) {
     const [{ widgets }, dispatch] = useSharedContext();
     // Current filter (search inside facet value).
@@ -23,8 +34,7 @@ export default function ({
     const { result } = widgets.get(id) || {};
     const data = (result && result.data) || [];
     const total = (result && result.total) || 0;
-    const [q, setQ] = useState(true);
-    const pq = q? {"geo_bounding_box":{"point_location":{"top_left":{"lat":"56.001111490713704","lon":"5.239240457720005"},"bottom_right":{"lat":"44.87490315978512","lon":"13.720685770220006"}}}}: { bool: { should: [] } }
+    const query = constructGeoQuery(field, value)
 
     // Update widgets properties on state change.
     useEffect(() => {
@@ -35,12 +45,19 @@ export default function ({
             needsConfiguration: true,
             isFacet: true,
             wantResults: false,
-            query: pq,
+            query: query,
             value,
-            configuration: { size, filterValue, fields, filterValueModifier },
+            configuration: { size, filterValue, fields: ["foo.baz"], undefined },
             result: data && total ? { data, total } : null
         });
-    }, [size, filterValue, value, q]);
+    }, [size, filterValue, value]);
+
+    // only update value if the provided value (initialValue) is different from the current
+    useEffect(() => {
+        if (JSON.stringify(initialValue) != JSON.stringify(value)) {
+            setValue(initialValue)
+        }
+    })
 
     // If widget value was updated elsewhere (ex: from active filters deletion)
     // We have to update and dispatch the component.
@@ -55,24 +72,8 @@ export default function ({
     function isValueReady() {
         return !widgets.get(id) || widgets.get(id).value == value;
     }
-
-    // On checkbox status change, add or remove current agg to selected
-    function handleChange(item, checked) {
-        const newValue = checked
-            ? [...new Set([...value, item.key])]
-            : value.filter(f => f !== item.key);
-        setValue(newValue);
-    }
-
-    // Is current item checked?
-    function isChecked(item) {
-        return value.includes(item.key);
-    }
-
     return (
         <div className="react-es-facet">
-            <button onClick={() => setQ(!q)}>hello</button>
-
         </div>
     );
 }
