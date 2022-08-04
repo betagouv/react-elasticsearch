43884bc48dd0e341095634f7fe951277a172f4e3
danjer <d.h.vandenakker@live.nl>
2022-08-04 12:14:32 +0200

    refactored geofacet

--- src/GeoFacet.js
+++ src/GeoFacet.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { toTermQueries } from "./utils";
 import { useSharedContext } from "./SharedContextProvider";
-import { isArray } from "lodash";
 
 const constructGeoQuery = (field, shapeValue) => {
     if (Array.isArray(shapeValue)) {
@@ -20,20 +18,10 @@ const constructGeoQuery = (field, shapeValue) => {
 
 export default function ({
     id,
-    initialValue,
+    value,
     field
 }) {
     const [{ widgets }, dispatch] = useSharedContext();
-    // Current filter (search inside facet value).
-    const [filterValue, setFilterValue] = useState("");
-    // Number of itemns displayed in facet.
-    const [size, setSize] = useState(1);
-    // The actual selected items in facet.
-    const [value, setValue] = useState(initialValue || []);
-    // Data from internal queries (Elasticsearch queries are performed via Listener)
-    const { result } = widgets.get(id) || {};
-    const data = (result && result.data) || [];
-    const total = (result && result.total) || 0;
     const query = constructGeoQuery(field, value)
 
     // Update widgets properties on state change.
@@ -43,35 +31,12 @@ export default function ({
             key: id,
             needsQuery: true,
             needsConfiguration: true,
-            isFacet: true,
-            wantResults: false,
             query: query,
-            value,
-            configuration: { size, filterValue, fields: ["foo.baz"], undefined },
-            result: data && total ? { data, total } : null
+            value, 
+            configuration: { size: 0, filterValue: "", fields: [field], undefined },
         });
-    }, [size, filterValue, value]);
+    }, [value]);
 
-    // only update value if the provided value (initialValue) is different from the current
-    useEffect(() => {
-        if (JSON.stringify(initialValue) != JSON.stringify(value)) {
-            setValue(initialValue)
-        }
-    })
-
-    // If widget value was updated elsewhere (ex: from active filters deletion)
-    // We have to update and dispatch the component.
-    useEffect(() => {
-        widgets.get(id) && setValue(widgets.get(id).value);
-    }, [isValueReady()]);
-
-    // Destroy widget from context (remove from the list to unapply its effects)
-    useEffect(() => () => dispatch({ type: "deleteWidget", key: id }), []);
-
-    // Checks if widget value is the same as actual value.
-    function isValueReady() {
-        return !widgets.get(id) || widgets.get(id).value == value;
-    }
     return (
         <div className="react-es-facet">
         </div>
